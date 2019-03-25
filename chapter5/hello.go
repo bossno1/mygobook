56e629a5869e521331ff106689424705ed2ca76a
8750h <249952>
2019-03-26 00:39:41 +0800

    1

--- chapter5/hello.go
+++ chapter5/hello.go
@@ -4,13 +4,32 @@ import (
     "io"
     "log"
     "net/http"
+    "fmt"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
     io.WriteString(w, "Hello world!")
 }
 
+type Integer int
+    
+func (a Integer) Less(b Integer) bool{
+    return a < b
+}
+func (a *Integer) Add (b Integer) {
+    *a += b
+}
 func main() {
+    var a Integer = 1 
+    if a.Less(2){
+        fmt.Println(a, " less 2 ")
+    }
+    a.Add(5);
+    fmt.Println(a);
+    var b1 = [3] int{1,2,3}
+    var b = &b1
+    b[1]++
+    fmt.Println(b1, *b)
     http.HandleFunc("/hello", helloHandler)
     err := http.ListenAndServe(":8080", nil)
     if err != nil {
--- chapter5/sqllx2.go
+++ chapter5/sqllx2.go
@@ -11,6 +11,29 @@ import (
     "github.com/jmoiron/sqlx"
 )
 var schema = `
+--可控病种列表（启用标志+有相关诊断）
+--select distinct 可控病种ID,可控病种自编码,受控病种 from v_cp_kkbz where ICD10_CODE = 'xxxxx'
+create table doctmark_cp_diff3
+(
+    ID        numeric               identity,
+    autonumb  int                   not null,
+    ddate     datetime              not null,
+    sdate     varchar(8)            not null,
+    operid    int                   not null,
+    hospid    int                   not null,
+    mediid    int                   not null,
+    mediname  varchar(255)          not null,
+    Reasonid  int                   not null,
+    Reason    varchar(255)          not null,
+    constraint PK_DOCTMARK_CP_DIFF primary key (ID)
+)
+;
+
+/* ============================================================ */
+/*   Index: doctmark_cp_diff_i1                                 */
+/* ============================================================ */
+create index doctmark_cp_diff_i3 on doctmark_cp_diff3 (autonumb, sdate)
+;
 CREATE TABLE person (
     first_name varchar(50),
     last_name  varchar(50),
@@ -53,7 +76,7 @@ func main() {
 	fmt.Println( u.String())
     //db, err := sqlx.Connect("sqlserver", u.String()) //"sqlserver://sa:146-164-156-@localhost:51798?database=his_yb&connection+timeout=30")
     //注意连接SQL SERVER 2008R2出错（可能需要打SP3补丁），而SQL SERVER 2017  port:52813 没有问题  
-    db, err := sqlx.Connect("sqlserver", "sqlserver://sa:146-164-156-@127.0.0.1:51798?database=master;encrypt=disable;app name=tqtest")
+    db, err := sqlx.Connect("sqlserver", "sqlserver://sa:146-164-156-@127.0.0.1:52813?database=master;encrypt=disable;app name=tqtest")
 	//db, err := sqlx.Connect("sqlserver", "server=192.168.31.144;port=51798;user id=sa;password=146-164-156-;database=his_yb") // ://sa:146-164-156-@localhost:51798?database=his_yb&connection+timeout=30"") //"")
 	//server=localhost\\SQLExpress;user id=sa;database=master;app name=MyAppName
     if err != nil {
@@ -64,20 +87,36 @@ func main() {
     // exec the schema or fail; multi-statement Exec behavior varies between
     // database drivers;  pq will exec them all, sqlite3 won't, ymmv
     db.MustExec(schema)
+    /*
+    以下如果用dbMustExec 则没有事务
     
+    
+
+    db.MustExec("INSERT INTO person (first_name, last_name, email) VALUES (@p1, @p2, @p3)", "Jason", "Moiron", "jmoiron@jmoiron.net")
+    db.MustExec("INSERT INTO person (first_name, last_name, email) VALUES (@p1, @p2, @p3)", "John", "Doe", "johndoeDNE@gmail.net")
+    */
+
+    /*
+    以下用tx 则是有事务
+    */
     tx := db.MustBegin()
-    tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES ($1, $2, $3)", "Jason", "Moiron", "jmoiron@jmoiron.net")
-    tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES ($1, $2, $3)", "John", "Doe", "johndoeDNE@gmail.net")
-    tx.MustExec("INSERT INTO place (country, city, telcode) VALUES ($1, $2, $3)", "United States", "New York", 1)
-    tx.MustExec("INSERT INTO place (country, telcode) VALUES ($1, $2)", "Hong Kong", 852)
-    tx.MustExec("INSERT INTO place (country, telcode) VALUES ($1, $2)", "Singapore", 65)
+    tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES (@p1, @p2, @p3)", "Jason", "Moiron", "jmoiron@jmoiron.net")
+    tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES (@p1, @p2, @p3)", "John", "Doe", "johndoeDNE@gmail.net")
+    tx.MustExec("INSERT INTO place (country, city, telcode) VALUES (@p1, @p2, @p3)", "United States", "New York", 1)
+    tx.MustExec("INSERT INTO place (country, telcode) VALUES (@p1, @p2)", "Hong Kong", 852)
+    tx.MustExec("INSERT INTO place (country, telcode) VALUES (@p1, @p2)", "Singapore", 65)
     //Named queries can use structs, so if you have an existing struct (i.e. person := &Person{}) that you have populated, you can pass it in as &person
-    tx.NamedExec("INSERT INTO person (first_name, last_name, email) VALUES (:first_name, :last_name, :email)", &Person{"Jane", "Citizen", "jane.citzen@example.com"})
+    //tx.NamedExec("INSERT INTO person (first_name, last_name, email) VALUES (:first_name, :last_name, :email)", &Person{"Jane", "Citizen", "jane.citzen@example.com"})
+    // var FirstName, SecondNames string
+	// fmt.Printf("Please enter your full name: ")
+	// fmt.Scanln(&FirstName, &SecondNames) 
+	// fmt.Printf("Hi %s %s!\n", FirstName, SecondNames)
     tx.Commit()
 
     // Query the database, storing results in a []Person (wrapped in []interface{})
     people := []Person{}
     db.Select(&people, "SELECT * FROM person ORDER BY first_name ASC")
+    fmt.Printf("count: %d ", len(people))
     jason, john := people[0], people[1]
 
     fmt.Printf("%#v\n%#v", jason, john)
