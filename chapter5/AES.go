e085b7ed67b710470061e85f1de8574a6e62a00a
8750h <249952>
2019-05-19 11:39:32 +0800

    增加 unaes

--- chapter5/AES.go
+++ chapter5/AES.go
@@ -16,34 +16,98 @@ import (
 )
 func main() {
     /*
-    *src 要加密的字符串
+    *src 要加密或解密的字符串
     *key 用来加密的密钥 密钥长度可以是128bit、192bit、256bit中的任意一个
     *16位key对应128bit
-	 */
-	if len(os.Args) != 3 {
-		panic("使用方式：aes.exe key text")
-	}
-	key := os.Args[1]
-	src := os.Args[2]
-	 
-	//src := "85:6CFB29781341DDB2B1DD1E88F8C5B39FEC6CB8FCFADA86B01F506C6F9B45EE68:1"
-    //key := "ae4ead79e2bb40d69f163717b61e7984"
 
+    aes ae4ead79e2bb40d69f163717b61e7984 85:6CFB29781341DDB2B1DD1E88F8C5B39FEC6CB8FCFADA86B01F506C6F9B45EE68:1
+    aes ae4ead79e2bb40d69f163717b61e7984 1
+    aes d ae4ead79e2bb40d69f163717b61e7984 YEtumQjPD6Vqq4gn2UT6qg==
+    aes d ae4ead79e2bb40d69f163717b61e7984 avsmYXH2kwROgfn713Igddp0ubo3le7JCFzg9lGA5sGiQu3fUwlQn/Ta8Uw9CDarV/COPN+KAYjrt2g5IzNmHCSk2eglQpK7chWI48AIpYI=
+     */
+     
+     switch len(os.Args) {
+        case 4 :
+            key := os.Args[2]
+            crypted := os.Args[3]
+           // fmt.Println([]byte(crypted), string([]byte(crypted)));
+            //AesDecrypt([]byte(crypted), []byte(key))
+            iReturn := saveAesDecrypt(crypted, []byte(key))
+            os.Exit(iReturn)
+        case 3:
+            key := os.Args[1]
+            src := os.Args[2]
+            iReturn := saveAesEncrypt(src, key)
+            os.Exit(iReturn)
+        default:
+            panic("使用方式：aes.exe key text 或解密 aes.exe d key text " )
+     }
+}
+
+func saveAesEncrypt(src , key string) int {
+    //将加密后的结果写到txt  
 	crypted := AesEncrypt(src, key)
 	userFile := "AES.txt"
     fout, err := os.Create(userFile)        
     if err != nil {
         fmt.Println(userFile, err)
-        return
+        return -1
+    }
+    defer fout.Close()
+ 
+    fout.Write([]byte(base64.StdEncoding.EncodeToString(crypted) ))
+    fout.Write([]byte(key))
+    //fout.Writeln([]byte(base64.StdEncoding.EncodeToString(crypted) ))
+    
+    fmt.Println("------end---------")
+
+    // src := AesDecrypt( []byte("YEtumQjPD6Vqq4gn2UT6qg=="), []byte(key))
+    // fout.Write([]byte('解密----'))
+    // fout.Write(src)
+    //  iReturn := saveAesDecrypt( []byte("YEtumQjPD6Vqq4gn2UT6qg==")  , []byte(key))
+   //  fmt.Println( iReturn)
+    return 100
+}
+
+func AesEncrypt(src, key string) []byte {
+    block, err := aes.NewCipher([]byte(key))
+    if err != nil {
+        fmt.Println("key error1", err)
+    }
+    if src == "" {
+        fmt.Println("plain content empty")
+    }
+    ecb := NewECBEncrypter(block)
+    content := []byte(src)
+    content = PKCS5Padding(content, block.BlockSize())
+    crypted := make([]byte, len(content))
+    ecb.CryptBlocks(crypted, content)
+    // 普通base64编码加密 区别于urlsafe base64
+    fmt.Println("base64 result:", base64.StdEncoding.EncodeToString(crypted))
+
+    fmt.Println("base64UrlSafe result:", Base64UrlSafeEncode(crypted))
+    //测试解密
+    //AesDecrypt(crypted, []byte(key)) 
+    return crypted
+}
+
+func saveAesDecrypt(crypted string, key []byte) int {
+    //将解密后的结果写到txt  
+
+    decrypted, _ := base64.StdEncoding.DecodeString(crypted)
+    
+	src := AesDecrypt(decrypted, key)
+	userFile := "AES.txt"
+    fout, err := os.Create(userFile)        
+    if err != nil {
+        fmt.Println(userFile, err)
+        return -1
     }
     defer fout.Close()
       
-       // fout.WriteString("Just a test!\r\n")
-    fout.Write([]byte(base64.StdEncoding.EncodeToString(crypted) ))
-    //}
-   // AesDecrypt(crypted, []byte(key))
-    //Base64URLDecode("39W7dWTd_SBOCM8UbnG6qA")
-    os.Exit(100)
+    fout.Write(src)
+   
+    return 100
 }
 
 func Base64URLDecode(data string) ([]byte, error) {
@@ -64,6 +128,9 @@ func Base64UrlSafeEncode(source []byte) string {
 }
 
 func AesDecrypt(crypted, key []byte) []byte {
+   // fmt.Print(string(crypted))
+   // fmt.Print(string(key))
+    
     block, err := aes.NewCipher(key)
     if err != nil {
         fmt.Println("err is:", err)
@@ -74,26 +141,6 @@ func AesDecrypt(crypted, key []byte) []byte {
     origData = PKCS5UnPadding(origData)
     fmt.Println("source is :", origData, string(origData))
     return origData
-}
-
-func AesEncrypt(src, key string) []byte {
-    block, err := aes.NewCipher([]byte(key))
-    if err != nil {
-        fmt.Println("key error1", err)
-    }
-    if src == "" {
-        fmt.Println("plain content empty")
-    }
-    ecb := NewECBEncrypter(block)
-    content := []byte(src)
-    content = PKCS5Padding(content, block.BlockSize())
-    crypted := make([]byte, len(content))
-    ecb.CryptBlocks(crypted, content)
-    // 普通base64编码加密 区别于urlsafe base64
-    fmt.Println("base64 result:", base64.StdEncoding.EncodeToString(crypted))
-
-    fmt.Println("base64UrlSafe result:", Base64UrlSafeEncode(crypted))
-    return crypted
 }
 
 func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
